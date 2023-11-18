cb147af07bce8cf0bfbe9384fe3d80726ff07e98
brunnogonzalez <bird.tec.gonzalez@gmail.com>
2023-11-18 17:27:51 -0300

    feat: start search bar implementation

--- app/src/main/java/com/bgaprojects/aluvery/screens/HomeScreen.kt
+++ app/src/main/java/com/bgaprojects/aluvery/screens/HomeScreen.kt
@@ -1,12 +1,25 @@
 package com.bgaprojects.aluvery.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bgaprojects.aluvery.components.ProductSection
@@ -14,15 +27,30 @@ import com.bgaprojects.aluvery.model.Product
 import com.bgaprojects.aluvery.sampledata.sampleSections
 import com.bgaprojects.aluvery.ui.theme.AluveryTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    MainColumn(sections)
+    var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
+
+    Column {
+        TextField(
+            value = value,
+            onValueChange = { value = it },
+            label = { Text("Enter text") },
+            maxLines = 2,
+            textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 16.dp)
+        )
+        MainContent(sections)
+    }
 }
 
 @Composable
-private fun MainColumn(sections: Map<String, List<Product>>) {
+private fun MainContent(sections: Map<String, List<Product>>) {
     LazyColumn(
         Modifier
             .fillMaxSize(),
