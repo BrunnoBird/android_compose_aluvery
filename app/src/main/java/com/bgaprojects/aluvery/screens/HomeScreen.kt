644873a956a6cb6c71bc5391711abfef0222a09d
brunnogonzalez <bird.tec.gonzalez@gmail.com>
2023-11-18 16:58:40 -0300

    feat: start screen

--- app/src/main/java/com/bgaprojects/aluvery/screens/HomeScreen.kt
+++ app/src/main/java/com/bgaprojects/aluvery/screens/HomeScreen.kt
@@ -4,14 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bgaprojects.aluvery.components.ProductItem
 import com.bgaprojects.aluvery.components.ProductSection
 import com.bgaprojects.aluvery.model.Product
 import com.bgaprojects.aluvery.sampledata.sampleSections
@@ -21,27 +18,7 @@ import com.bgaprojects.aluvery.ui.theme.AluveryTheme
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    DesafioGrid(sections)
-}
-
-@Composable
-private fun DesafioGrid(sections: Map<String, List<Product>>) {
-    LazyVerticalGrid(
-        modifier = Modifier
-            .fillMaxSize(),
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        for (section in sections) {
-            val allProducts: List<Product> = section.value
-
-            allProducts.forEach {
-                item { ProductItem(product = it) }
-            }
-        }
-    }
+    MainColumn(sections)
 }
 
 @Composable
@@ -70,8 +47,7 @@ private fun MainColumn(sections: Map<String, List<Product>>) {
 private fun HomeScreenPreview() {
     AluveryTheme {
         Surface {
-//            HomeScreen(sampleSections)
-            DesafioGrid(sampleSections)
+            HomeScreen(sampleSections)
         }
     }
 }
